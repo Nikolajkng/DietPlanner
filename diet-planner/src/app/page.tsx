66fb1bd7a9497be079d6f3bf43b2ec39f54ea3fd
Nikolaj <102536857+Nikolajkng@users.noreply.github.com>
2025-06-24 21:20:22 +0200

    moved nav and footer to layout

--- diet-planner/src/app/page.tsx
+++ diet-planner/src/app/page.tsx
@@ -4,17 +4,7 @@ import Navbar from "./components/navbar";
 export default function Home() {
   return (
     <>
-      <Navbar />
-      <div>
-        <main>
-          <div className="flex justify-around">
-            <h1 className="text-4xl font-bold">Welcome to Diet Planner</h1>
-          </div>
-        </main>
-        <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-          Footer
-        </footer>
-      </div>
+      Forsiden - Home
     </>
   );
 }
