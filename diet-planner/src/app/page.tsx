a41f65b1b6b8986636dc8a410fbce0ca55315888
Martin <Martintcao@hotmail.com>
2025-06-24 21:14:58 +0200

    Changed Navbar routing

--- diet-planner/src/app/page.tsx
+++ diet-planner/src/app/page.tsx
@@ -1,18 +1,20 @@
 import Image from "next/image";
+import Navbar from "./components/navbar";
 
 export default function Home() {
   return (
-    <div>
-      <main>
-        <div className="flex justify-around">
-          <h1 className="text-4xl font-bold">
-            Welcome to Diet Planner
-          </h1>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        Footer
-      </footer>
-    </div>
+    <>
+      <Navbar />
+      <div>
+        <main>
+          <div className="flex justify-around">
+            <h1 className="text-4xl font-bold">Welcome to Diet Planner</h1>
+          </div>
+        </main>
+        <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+          Footer
+        </footer>
+      </div>
+    </>
   );
 }
